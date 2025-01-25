c32cad7eb156c5b42cfa9c8d8efe3923bf8adcb9
Mateus Nardo <mateusdnm@hotmail.com>
2025-01-26 00:55:52 +0100

    feat: add previous and next buttons as fixed elements

--- web-reader/astro.config.mjs
+++ web-reader/astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
 });
