57987228a558b4d6ce5ef2505c2cf1ebd73739b6
Mateus Nardo <mateusdnm@hotmail.com>
2025-01-26 01:28:40 +0100

    feat: implement dark mode and enhance content configuration

--- web-reader/src/content.config.ts
+++ web-reader/src/content.config.ts
@@ -4,6 +4,8 @@ const englishTranslationDrafts = defineCollection({
   schema: z.object({
     title: z.string().optional(),
     date: z.string().optional(),
+    youtubeEmbedLink: z.string().optional(),
+    youtubeEmbedTitle: z.string().optional(),
   }),
 });
 
